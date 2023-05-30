398ebd63c6402b32362fb937e3530d528dbb56e4
znikolaj046 <62992034+znikolaj046@users.noreply.github.com>
2023-05-30 22:12:29 +0300

    Добавлены ссылки слева

--- src/components/CategoryBar.js
+++ src/components/CategoryBar.js
@@ -1,10 +1,12 @@
 import { ListGroup } from 'react-bootstrap'
+import { Link, useNavigate } from 'react-router-dom'
 import { useContext } from 'react'
 import { AppContext } from './AppContext.js'
 import { observer } from 'mobx-react-lite'
 
 const CategoryBar = observer(() => {
     const { catalog } = useContext(AppContext)
+    const navigate = useNavigate()
 
     const handleClick = (id) => {
         if (id === catalog.category) {
@@ -14,19 +16,21 @@ const CategoryBar = observer(() => {
         }
     }
 
+/*
+    <ListGroup.Item
+        key={item.id}
+        active={item.id === catalog.category}
+        onClick={() => handleClick(item.id)}
+        style={{cursor: 'pointer'}}
+    >
+        {item.name}
+    </ListGroup.Item>
+*/
+
     return (
-        <ListGroup>
-            {catalog.categories.map(item =>
-                <ListGroup.Item
-                    key={item.id}
-                    active={item.id === catalog.category}
-                    onClick={() => handleClick(item.id)}
-                    style={{cursor: 'pointer'}}
-                >
-                    {item.name}
-                </ListGroup.Item>
-            )}
-        </ListGroup>
+        catalog.categories.map(item =>
+            <div key={item.id}><Link to={'/catalog/' + item.alias}>{item.name}</Link></div>                
+        )
     )
 })
 
